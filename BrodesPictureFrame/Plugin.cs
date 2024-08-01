df2d69c540b743abab811e6e3f8d13b24093c320
brodes (also known as BR0DES) <blhennel@gmail.com>
2024-08-01 09:27:36 -0600

    Add project files.

---
+++ BrodesPictureFrame/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BrodesPictureFrame
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        AssetBundle bundle;
+
+        void Start()
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("BrodesPictureFrame.res.mynewbundle");
+            bundle = AssetBundle.LoadFromStream(stream);
+            if (stream == null)
+            {
+                Debug.LogError("STREAM IS NULL. NOT LOADING ASSETS");
+            }
+            stream.Close();
+
+            GameObject untitled0 = Instantiate(bundle.LoadAsset<GameObject>("untitled"));
+        }
+    }
+}
---
+++ BrodesPictureFrame/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace BrodesPictureFrame
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.brodes.gorillatag.brodespictureframe";
+        public const string Name = "BrodesPictureFrame";
+        public const string Version = "1.0.0";
+    }
+}
